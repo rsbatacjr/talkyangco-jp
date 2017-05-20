f96619a6a3bb0c03e03119bcf90e50a358b61fdd
rey <reybatacjr@gmail.com>
2017-05-20 12:54:21 +0800

    Initial commit

---
+++ footer.php
@@ -0,0 +1,135 @@
+			<div class="row">
+				<footer>
+					<div id="footer-menu">
+						<div class="container">
+							<div class="row">
+								<div class="col-md-2">
+									<ul>
+										<li><a href="">紹介</a></li>
+										<li>&nbsp;</li>
+										<li><a href="">ABOUT US</a></li>
+										<li><a href="">あいさつ</a></li>
+										<li><a href="">TALKの良さ</a></li>
+										<li><a href="">TALKの場所</a></li>
+									</ul>
+								</div>
+								<div class="col-md-2">
+									<ul>
+										<li><a href="">過程</a></li>
+										<li>&nbsp;</li>
+										<li><a href="">手順</a></li>
+										<li><a href="">準備するもの</a></li>
+										<li><a href="">FAQ</a></li>
+										<li><a href="">保険について</a></li>
+										<li><a href="">バギオへの行き方 </a></li>
+										<li><a href="">キャンペーン</a></li>
+									</ul>
+								</div>
+								<div class="col-md-2">
+									<ul>
+										<li><a href="">入学費</a></li>
+										<li>&nbsp;</li>
+										<li><a href="">授業料</a></li>
+										<li><a href="">お部屋タイプ</a></li>
+										<li><a href="">多種多様な料金設定</a></li>
+										<li><a href="">IELTS  公式テスト日程</a></li>
+										<li><a href="">TOEIC 公式テスト日程</a></li>
+										<li><a href="">祝日</a></li>
+										<li><a href="">ピックアップサービス日程</a></li>
+										<li><a href="">返金について</a></li>
+										<li><a href="">E-Application(Register now)</a></li>
+									</ul>
+								</div>
+								<div class="col-md-2">
+									<ul>
+										<li><a href="">コースについて</a></li>
+										<li>&nbsp;</li>
+										<li><a href="">ESL 421, 521, 611</a></li>
+										<li><a href="">Intensive IELTS</a></li>
+										<li><a href="">Intensive TOEIC</a></li>
+										<li><a href="">IELTS 保証コース</a></li>
+										<li><a href="">TOEIC 保証コース</a></li>
+										<li><a href="">パワースピーキング</a></li>
+										<li><a href="">ワーキングホリデー</a></li>
+										<li><a href="">ファミリーコース ESL</a></li>
+										<li><a href="">相談</a></li>
+									</ul>
+								</div>
+								<div class="col-md-2">
+									<ul>
+										<li><a href="">留学プラン</a></li>
+										<li>&nbsp;</li>
+										<li><a href="">TOEIC test</a></li>
+										<li><a href="">IELTS test</a></li>
+										<li><a href="">パワースピーキング</a></li>
+										<li><a href="">短期間留学(1~3週間)</a></li>
+										<li><a href="">長期間留学</a></li>
+										<li><a href="">手ごろな費用</a></li>
+										<li><a href="">総合コース</a></li>
+									</ul>
+								</div>
+								<div class="col-md-2">
+									<ul>
+										<li><a href="">コミュニティ</a></li>
+										<li>&nbsp;</li>
+										<li><a href="">注意</a></li>
+										<li><a href="">生徒の経験談</a></li>
+										<li><a href="">TALKの食事</a></li>
+										<li><a href="">学校外</a></li>
+										<!-- <li><a href="">Best Student &amp; Teacher</a></li> -->
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="container">
+						<div class="row" style="margin-top: 30px; margin-bottom: 30px;">
+							<div class="footer-icons col-xs-6 col-sm-3 text-center">
+								<img src="<?php echo THEME_URI ?>/img/bureau-immigration.png" style="width: 90px; height: 90px;">
+								<p>SSP<br>(Special Study Permit)</p>
+							</div>
+							<div class="footer-icons col-xs-6 col-sm-3 text-center">
+								<img src="<?php echo THEME_URI ?>/img/tesda.png" style="width: 90px; height: 90px;">
+								<p>TESDA</p>
+							</div>
+							<div class="footer-icons col-xs-6 col-sm-3 text-center">
+								<img src="<?php echo THEME_URI ?>/img/pcc.png" style="width: 90px; height: 90px;">
+								<p>Accreditation<br>certificate of<br>TVET </p>
+							</div>
+							<div class="footer-icons col-xs-6 col-sm-3 text-center">
+								<img src="<?php echo THEME_URI ?>/img/besa.png" style="width: 90px; height: 90px;">
+								<p>Baguio English<br>Schools Association</p>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-6">
+								<p>
+									Yangco Center<br>
+									Tel : +63-74-442-6012<br>
+									Address : #37 yangco Rd., Upper General Luna Rd., Baguio City, 2600 Philippines<br>
+									President Kenneth : +63-917-568-9161
+								</p>
+							</div>
+							<div class="col-md-6">
+								<p>
+									EnE Center<br>
+									Tel : +63-74-442-6012<br>
+									Address : Lot A, Dacanay Building, Marcos Highway, Baguio City, 2600 Philippines<br>
+									President Louis : +63-917-688-8656
+								</p>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12 text-center">
+								<p><br/><br/>
+								<a href="mailto:baguiotalk@gmail.com">baguiotalk@gmail.com</a> &copy; <i>Canitalk Academy Inc. All rights reserved.</i></p>
+							</div>
+						</div>
+					</div> 
+				</footer>
+			</div>
+		</div>
+	</body>
+	<script type="text/javascript" src="<?php echo THEME_URI ?>/js/jquery.min.js"></script>
+	<script type="text/javascript" src="<?php echo THEME_URI ?>/js/bootstrap.min.js"></script>
+</html>
---
+++ home.php
@@ -0,0 +1,274 @@
+<?php
+get_header();
+?>
+<div class="container">
+	<div class="row">
+		<div id="qualified-programs-container" class="col-xs-12 col-md-8">
+			<div id="qualified-programs">
+				<div class="mask">
+					<h1>最高のプログラム</h1>
+				<p style="max-width: 400px;">TALKアカデミーは英語を幅広く学べる学校です。
+TOEICやIELTSのテスト対策はもちろんの事、これから欧米諸国に留を考えている人にとって最適な学校です。</p>
+				<div id="course-builder-container">
+					<div class="row">
+						<div class="col-xs-12">
+							<strong>&nbsp;コース :</strong>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-12 col-sm-6 col-md-3">
+							<div class="course-builder">
+								<strong>スピーキングコース</strong>
+								<p>ESL 521</p>
+							</div>
+						</div>
+						<div class="col-xs-12 col-sm-6 col-md-3">
+							<div class="course-builder">
+								<strong>就職活動に有利なTOEIC</strong>
+								<p>Intensive TOEIC</p>
+							</div>
+						</div>
+						<div class="col-xs-12 col-sm-6 col-md-3">
+							<div class="course-builder">
+								<strong>外国での大学入学や<br>永住を考えている方</strong>
+								<p>Intensive IELTS</p>
+							</div>
+						</div>
+						<div class="col-xs-12 col-sm-6 col-md-3">
+							<div class="course-builder">
+								<strong>親子で留学</strong>
+								<p>Family ESL</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+				</div>
+			</div>
+		</div>
+		<div id="about-container" class="col-xs-12 col-sm-6 col-md-4">
+			<div id="about">
+				<div class="mask">
+					<h2>TALKアカデミーについて</h2>
+					<div id="about-normal">
+						<p>クラスルームや宿舎</p>
+						<span class="plus-sign">+</span>
+						<div class="clearfix"></div>
+					</div>
+					<div id="about-hover">
+						<p>
+						TALKアカデミーに留学する前に、3D映像にて当学校のクラスルームやお部屋の様子を見ることができます。
+事前に確認することで、より正確な情報を入手し、確かな留学を送れることをサポートします。
+						</p>
+						<span style="position: absolute;bottom: 10px;right: 10px;display: inline-block;margin-bottom: 0;" class="next-button"></span>
+						<div class="clearfix"></div>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+		<div id="experience-container" class="col-xs-12 col-sm-6 col-md-4">
+			<div id="experience">
+				<div class="mask">
+					<h2>生徒経験談</h2>
+					<div id="experience-normal">
+						<p>TALKで今までにない経験をしましょう</p>
+						<span class="plus-sign">+</span>
+					</div>
+					<div id="experience-hover">
+						<p>
+							当学校に留学された生徒さん達の意見を聞き、
+実際の生活を知ることができます。
+彼らの意見を参考に、フィリピンでの生活をイメージしましょう。
+						</p>
+						<span style="position: absolute;bottom: 10px;right: 10px;display: inline-block;margin-bottom: 0;" class="next-button"></span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div id="notice-container" class="col-xs-12 col-md-4">
+			<div id="notice">
+				<strong>注意</strong>
+				<p>
+					留学を決定される前に、TALKアカデミーについてのニュースをチェックしましょう。留学に役立つ資料が載っています。
+				</p>
+				<div id="notice-list-container">
+					<div class="row">
+						<div class="col-xs-12">
+							<span id="notice-plus">+</span>
+							<span class="next-button"></span>
+						</div>
+					</div>
+					<ul id="notice-list">
+						<li><a href="#">5월 입학가능일 안내</a></li>
+						<li><a href="#">노용이 화난 날(Warning)</a></li>
+						<li><a href="#">노용이 소리 질른 날..ㅋㅋ</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div id="plans-container" class="col-xs-12 col-md-8">
+			<div id="plans">
+				<div class="mask">
+					<strong>豊富なコース選択</strong>
+					<div id="plan-list-container" class="row">
+						<div class="row">
+							<div class="col-xs-12">
+								<div class="plan-list-parent col-xs-12 col-md-4">
+									<div class="plan-list">
+										<div class="plan-list-normal">
+											<p><br>IELTS<br>5.0, 5.5, 6.0</p>
+											<span class="plus-sign">+</span>
+										</div>	
+										<div class="plan-list-hover">
+											<p>海外の大学入学や、永住を考えている人へお勧めするコースです。<br>IELTS コース</p>
+											<span class="next-button-black"></span>
+										</div>
+									</div>
+								</div>
+								<div class="plan-list-parent col-xs-12 col-md-4">
+									<div class="plan-list">
+										<div class="plan-list-normal">
+											<p><br>パワースピーキング</p>
+											<span class="plus-sign">+</span>
+										</div>	
+										<div class="plan-list-hover">
+											<p>文法や語彙に自信があっても、スーキングに自信がない人向け、スピーキングに特化したspeaking コース</p>
+											<span class="next-button-black"></span>
+										</div>
+									</div>
+								</div>
+								<div class="plan-list-parent col-xs-12 col-md-4">
+									<div class="plan-list">
+										<div class="plan-list-normal">
+											<p><br>短期留学可能(1-3週間)</p>
+											<span class="plus-sign">+</span>
+										</div>	
+										<div class="plan-list-hover">
+											<p>英語を勉強する時間があまりない人や、どのように英語の学習を進めていいのかわからない人におすすめのコースです。
+短期留学コース</p>
+											<span class="next-button-black"></span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-xs-12">
+								<div class="plan-list-parent col-xs-12 col-md-4">
+									<div class="plan-list">
+										<div class="plan-list-normal">
+											<p><br>TOEIC<br>600, 700, 800, 900</p>
+											<span class="plus-sign">+</span>
+										</div>
+										<div class="plan-list-hover">
+											<p>フィリピンで最初にTOEIC点数保証コースを取り入れたTALKアカデミーでTOEICの点数上げましょう。
+TOEIC コース</p>
+											<span class="next-button-black"></span>
+										</div>
+									</div>
+								</div>
+								<div class="plan-list-parent col-xs-12 col-md-4">
+									<div class="plan-list">
+										<div class="plan-list-normal">
+											<p><br>オーストラリアやカナダで使う<br>英語をフィリピンで学習</p>
+											<span class="plus-sign">+</span>
+										</div>
+										<div class="plan-list-hover">
+											<p><br>大学に入学する前にもっと英語を身につけたい人にお勧めのコースです。</p>
+											<span class="next-button-black"></span>
+										</div>
+										
+									</div>
+								</div>
+								<div class="plan-list-parent col-xs-12 col-md-4">
+									<div class="plan-list">
+										<div class="plan-list-normal">
+											<p><br>費用を抑えてTALKに留学</p>
+											<span class="plus-sign">+</span>
+										</div>
+										<div class="plan-list-hover">
+											<p><br>最小限に留学費用を抑えたい方、<br>サポートします。</p>
+											<span class="next-button-black"></span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div id="best-place-container" class="col-xs-12 col-md-4">
+			<div id="best-place">
+				<div class="mask">
+					<strong>旅行先としても有名</strong>
+					<div id="best-place-list-container">
+						<ul id="best-place-list">
+							<li><a href="#">バギオの街</a></li>
+							<li><a href="#">１日旅</a></li>
+							<li><a href="#">宿泊旅行</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="campaign-container" class="col-xs-12 col-md-4">
+			<div id="campaign">
+				<strong>キャンペーン</strong>
+				<div id="campaign-list-container">
+					<ul id="campaign-list">
+						<li><a href="#">ネイティブ講師による　IELTSクラス</a></li>
+						<li><a href="#">300点アップ TOEIC 12週間コース</a></li>
+						<li><a href="#">TALKで公式TOEICテストを無料で受けれます。</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div id="calendar-container" class="col-xs-12 col-md-4">
+			<div id="calendar">
+				<strong>スケジュール</strong>
+				<div id="calendar-list-container">
+					<div class="row">
+						<div class="col-xs-12">
+							<div class="calendar-list-parent col-xs-6" style="border-right:1px solid #666; border-bottom: 1px solid #666">
+								<div class="calendar-list">
+									<p><br>祝日</p>
+								</div>
+							</div>
+							<div class="calendar-list-parent col-xs-6" style=" border-bottom: 1px solid #666">
+								<div class="calendar-list">
+									<p>ピックアップサービス</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-12">
+							<div class="calendar-list-parent col-xs-6" style="border-right:1px solid #666">
+								<div class="calendar-list">
+									<p><br>IELTS <br>公式テスト<br>日程</p>
+								</div>
+							</div>
+							<div class="calendar-list-parent col-xs-6">
+								<div class="calendar-list">
+									<p><br>TOEIC <br>公式テスト<br>日程</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="row">
+	<img src="<?php echo THEME_URI; ?>/img/sample-image1.png" class="img-responsive"/>
+</div>
+<?php 
+get_footer();
