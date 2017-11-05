c4dd03e01896759ae7e997004b5779b695445284
reynaldob <reynaldob@sonarlogic.biz>
2017-11-05 10:36:50 +0800

    Add category list 2 template.

---
+++ category-list-2.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * Template Name: Category List 2
+ */
+	get_header("mobile");
+?>
+<div class="container">
+
+	<div class="row">
+		<?php while (have_posts()) : the_post(); ?>
+			<div class="col-xs-12">
+				<h1><?php the_title(); ?></h1>
+			</div>
+	<?php endwhile; ?>
+	</div>
+	<div class="row">
+	<div class="col-xs-12">
+<?php
+listByPostType2();
+?>
+	</div>
+	</div>
+</div>
+<?php
+	get_footer("mobile");
