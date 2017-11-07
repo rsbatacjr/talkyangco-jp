463d1ad4789d7e055e49fed987183a2296c776da
reynaldob <reynaldob@sonarlogic.biz>
2017-11-07 19:57:28 +0800

    create page template for yangco esl

---
+++ page-top-image.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ * Template Name: Top Image
+ */
+	get_header();
+?>
+	<div class="row">
+		<?php while (have_posts()) : the_post(); ?>
+
+			<?php
+			if(has_post_thumbnail($loop->ID)){
+        		$thumbnail=wp_get_attachment_image_src(get_post_thumbnail_id($loop->ID),'single-post-thumbnail')[0];
+        	} else {
+	        	$thumbnail='';
+	        }
+	        ?>
+
+
+	        		<div class="container">
+					<br>
+					<br>
+	        			<div class="row">
+                            <div class="col-xs-12 col-sm-offset-3 col-sm-3">
+        						<button style="border-radius: 10px;border: 1px solid #000; padding: 15px; background-color: transparent;width: 90%;margin-bottom: 15px;" onclick="javascript:$(this).css({'background-color': 'rgb(0,176,240)'});window.location='http://talk-academy.jp/yangco-esl/'">Yangco センター</button>
+						    </div>
+						    <div class="col-xs-12 col-sm-3">
+						        <button style="border-radius: 10px;border: 1px solid #000; padding: 15px; background-color: transparent;width: 90%;margin-bottom: 15px;" onclick="javascript:$(this).css({'background-color': 'rgb(0,176,240)'});window.location='http://talk-academy.jp/ene-center-esl-program/'">ESL プログラム</button>
+						    </div>
+                        </div>
+                        <br>
+			<br>
+	        		</div>
+
+
+
+	        <div style="background-color:#000;background-image:url('<?=$thumbnail?>');background-size:cover;background-position:center;height: 400px; margin-bottom: 15px;position:relative;">
+				<div style="color:#fff;background-color:rgba(0,0,0,.4);position:absolute;top:100px;min-height:150px;width:100%;">
+					<div class="container">
+
+						<div class="col-xs-12 text-center">
+							<h1><?= the_title(); ?></h1>
+							<div class="col-xs-12">
+								<div style="border:1px solid #fff;padding: 20px; border-radius: 10px; color: #fff; font-size: 180%;display: inline-block;">ESL 421, 521, 611</div>
+							</div>
+						</div>
+						<div class="clearfix">&nbsp;</div>
+					</div>
+				</div>
+			</div>
+	        <?php
+			the_content();
+			?>
+	<?php endwhile; ?>
+	</div>
+<?php
+	get_footer();
